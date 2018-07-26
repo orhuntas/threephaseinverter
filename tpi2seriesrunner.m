11b8dd3e4fc2ae15cbf62f45119bfb9b031616ae
Orhun Tasoglu <orhuntasoglu@gmail.com>
2018-07-27 00:56:26 +0300

    two inverters connected in series, now utilizing matlab workspace for easier modification

---
+++ tpi2seriesrunner.m
@@ -0,0 +1,15 @@
+clear
+Rin=1e-16
+dcin=20
+fdclinkres=1e-6
+fdclinkcap=1e-2
+fswfreq=2050
+fma=0.9
+sdclinkres=1e-6
+sdclinkcap=1e-2
+sswfreq=2050
+sma=0.9
+FRload=5
+FLload=2e-3
+SRload=5
+SLload=2e-3

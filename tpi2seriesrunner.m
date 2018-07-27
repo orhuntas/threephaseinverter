995b5d9c77d2596810a261567d44991588ca9d99
Orhun Tasoglu <orhuntasoglu@gmail.com>
2018-07-27 17:39:55 +0300

    no message

--- tpi2seriesrunner.m
+++ tpi2seriesrunner.m
@@ -1,14 +1,22 @@
 clear
+freffreq = 2*pi*50
+sreffreq = 2*pi*50
+
+fswfreq = 2050
+sswfreq=2050
+
+fma=0.9
+sma=0.9
+
 Rin=1e-16
 dcin=20
+
 fdclinkres=1e-6
 fdclinkcap=1e-2
-fswfreq=2050
-fma=0.9
 sdclinkres=1e-6
 sdclinkcap=1e-2
-sswfreq=2050
-sma=0.9
+
+
 FRload=5
 FLload=2e-3
 SRload=5

de26a452159b869b08244a77bcfcd8fc51334986
Orhun Tasoglu <orhuntasoglu@gmail.com>
2018-08-09 14:53:22 +0300

    looks better, need some help about power calculations

--- tpi2parallelrunner.m
+++ tpi2parallelrunner.m
@@ -13,8 +13,6 @@ dcin=547.66
 
 fdclinkres=1e-6
 fdclinkcap=100e-6
-sdclinkres=1e-6
-sdclinkcap=100e-6
 
 Load_Real_Power = 4250;
 Load_Power_Factor = 0.9; 
@@ -22,16 +20,16 @@ Load_Apparent_Power = Load_Real_Power/Load_Power_Factor; %VA
 Load_Reactive_Power = Load_Apparent_Power*sin(acos(Load_Power_Factor));
 
 n = 2; %number of series modules
-SVll_rms = sma*dcin*0.612/n;
-SIline = Load_Apparent_Power/(SVll_rms*sqrt(3));
-SZload = SVll_rms/(SIline*sqrt(3));  %ohm total
-SRload = n*SZload*Load_Power_Factor;  %ohm total
-SXload = n*SZload*sin(acos(Load_Power_Factor)); %ohm total
+SVll_rms = 380;
+SZload = (SVll_rms)^2/(Load_Apparent_Power/3);
+SIline = Load_Apparent_Power/(3*SZload);
+SRload = SZload*Load_Power_Factor ;  %ohm total
+SXload = SZload*sin(acos(Load_Power_Factor)); %ohm total
 SLload = SXload/sreffreq;
 
-FVll_rms = fma*dcin*0.612/n;
-FIline = Load_Apparent_Power/(FVll_rms*sqrt(3));
-FZload = FVll_rms/(FIline*sqrt(3));  %ohm total
-FRload = n*FZload*Load_Power_Factor;  %ohm total
-FXload = n*FZload*sin(acos(Load_Power_Factor)); %ohm total
+FVll_rms = 380 ;
+FZload = (FVll_rms)^2/(Load_Apparent_Power/3); 
+FIline = Load_Apparent_Power/(3*FZload);
+FRload = FZload*Load_Power_Factor;  %ohm total
+FXload = FZload*sin(acos(Load_Power_Factor)); %ohm total
 FLload = FXload/freffreq;

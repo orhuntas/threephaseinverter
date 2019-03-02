f75bde7d9028cf1c156243cab302757f9a7a0bc4
Orhun Tasoglu <orhuntasoglu@gmail.com>
2019-03-02 18:53:05 +0300

    can locate open transistor fault

--- tpireloadedrunner.m
+++ tpireloadedrunner.m
@@ -22,8 +22,12 @@ Load_Power_Factor = 0.9;
 Load_Apparent_Power = Load_Real_Power/Load_Power_Factor; %VA
 Load_Reactive_Power = Load_Apparent_Power*sin(acos(Load_Power_Factor));
 
-topopen=10
-botopen=10
+atopopen=10
+abotopen=10
+btopopen=10
+bbotopen=10
+ctopopen=0.03
+cbotopen=10
 
 n = 1; %number of series modules
 SVll_rms = sma*dcin*0.612/n;
